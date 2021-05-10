ca6df945575b24d1ec68bd3f1af4ec5e884fdec0
koziolek151815 <koziolek151815x@gmail.com>
2021-05-10 15:21:21 +0200

    Step 5 (Message and listing negative numbers if there were any.)

--- src/main/java/com/futurestars/empik/EmpikController.java
+++ src/main/java/com/futurestars/empik/EmpikController.java
@@ -1,14 +1,17 @@
 package com.futurestars.empik;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class EmpikController {
     @PostMapping
-    public int Add(@RequestBody String numbers) {
+    public ResponseEntity<?> Add(@RequestBody String numbers) {
         if (numbers.startsWith("//")) {
             String delimiter = "[" + numbers.substring(2, 3) + "]";
             String stringToSplit = numbers.substring(5);
@@ -16,24 +19,38 @@ public class EmpikController {
                 throw new RuntimeException("Wrong delimiter");
             }
             List<String> splittedString = Arrays.asList(stringToSplit.split(delimiter + "|\r\n"));
-            return sumNumbersInList(splittedString);
+            return returnSumOrMessage(splittedString);
 
         } else {
             if (numbers.length() == 0) {
-                return 0;
+                return ResponseEntity
+                        .status(HttpStatus.OK)
+                        .body(0);
             }
             List<String> splittedString = Arrays.asList(numbers.split(";|\r\n"));
-            return sumNumbersInList(splittedString);
+            return returnSumOrMessage(splittedString);
         }
     }
 
-    public int sumNumbersInList(List<String> splittedString) {
+    public ResponseEntity<?> returnSumOrMessage(List<String> splittedString) {
+        List<String> negatives = new ArrayList<>();
         int sum = 0;
         for (String number : splittedString) {
             int parsed = Integer.parseInt(number);
+            if (parsed < 0) {
+                negatives.add(number);
+            }
             sum += parsed;
         }
-        return sum;
-    }
+        if (negatives.isEmpty()) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(sum);
 
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("negatives not allowed: " + negatives);
+        }
+    }
 }
