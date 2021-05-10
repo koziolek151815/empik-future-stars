8acb3452bc51ccac0cd9b6c744d92d0ffb99183d
koziolek151815 <koziolek151815x@gmail.com>
2021-05-10 13:27:14 +0200

    Microservice initiation

---
+++ src/main/java/com/futurestars/empik/EmpikController.java
@@ -0,0 +1,7 @@
+package com.futurestars.empik;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class EmpikController {
+    
+}
---
+++ src/test/java/com/futurestars/empik/EmpikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futurestars.empik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
