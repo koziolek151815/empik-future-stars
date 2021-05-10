fe3cf9e340f78f4ace1820d1639c48607161ef95
koziolek151815 <koziolek151815x@gmail.com>
2021-05-10 13:50:08 +0200

    Step 1 (Method can take up to two numbers, separated by commas)

--- src/main/java/com/futurestars/empik/EmpikController.java
+++ src/main/java/com/futurestars/empik/EmpikController.java
@@ -1,7 +1,30 @@
 package com.futurestars.empik;
+
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class EmpikController {
-    
+
+    @PostMapping
+    public int Add(@RequestBody String numbers) {
+        if (numbers.length() == 0) {
+            return 0;
+        }
+        List<String> splittedString = Arrays.asList(numbers.split(","));
+
+        if (splittedString.size() >= 3) {
+            throw new RuntimeException("Wrong number of parameters");
+        }
+        if (splittedString.size() == 1) {
+            return Integer.parseInt(splittedString.get(0));
+        } else {
+            int a = Integer.parseInt(splittedString.get(0));
+            int b = Integer.parseInt(splittedString.get(1));
+
+            return a + b;
+        }
+    }
 }
